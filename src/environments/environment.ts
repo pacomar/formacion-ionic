c7fbdf922026d2920eed3817d8afa2f5e90d9915
Francisco Martinez Espartero <francisco.mar.esp@gmail.com>
2020-03-17 11:42:58 +0100

    hide vars envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   superhero_api: 'https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/',
   firebase: {
-    apiKey: 'AIzaSyCbTmsML0CjLcxAdtjdBOHGhtu2AiDvkPM',
-    authDomain: 'ciberionicformation.firebaseapp.com',
-    databaseURL: 'https://ciberionicformation.firebaseio.com',
-    projectId: 'ciberionicformation',
-    storageBucket: 'ciberionicformation.appspot.com',
-    messagingSenderId: '309225380372',
-    appId: '1:309225380372:web:975b07f0750580a9596501'
+    apiKey: '--------------------',
+    authDomain: '--------------------',
+    databaseURL: '--------------------',
+    projectId: '--------------------',
+    storageBucket: '--------------------',
+    messagingSenderId: '--------------------',
+    appId: '--------------------'
   }
 };
 
